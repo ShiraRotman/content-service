6244baa503ec4feae8899c4a9c23038c5d899a44
David Meir-Levy <davidmeirlevy@gmail.com>
2019-08-25 16:20:22 +0300

    close init after finish

--- helpers/init.js
+++ helpers/init.js
@@ -23,18 +23,22 @@ const cat2 = new Category({
 });
 
 
+console.log('initiate content');
+
+
 Promise.all([
 	category.save(),
 	cat2.save(),
-]).then(async ([newCategory, secondCat]) => {
+])
+	.then(async ([newCategory, secondCat]) => {
 
-	await (new Post({
-		category: newCategory._id,
-		isPublic: true,
-		author: [],
-		title: 'Welcome to your new blog',
-		short: 'This is the first and demo post.<br>I hope you will enjoy this platform',
-		content: `<p>
+		await (new Post({
+			category: newCategory._id,
+			isPublic: true,
+			author: [],
+			title: 'Welcome to your new blog',
+			short: 'This is the first and demo post.<br>I hope you will enjoy this platform',
+			content: `<p>
 	This is an example post for greenpress platform.<br>
 	You can change it as you like, but this is a reference for you to create many more blog posts in the near future.
 </p><p>
@@ -49,17 +53,21 @@ Content service is responsible for menus, posts, categories (and comments in the
 Front Service is responsible for the client frontend and SSR, using Nuxt.js (+vue, vuex, vue-router..).<br>
 Hopefully on the time you read this line, there's already an admin service for you, so you can manage all of the content. :) 
 </p>`,
-		tags: [],
-	})).save();
+			tags: [],
+		})).save();
 
-	return (new Menu({
-		name: 'main',
-		links: [{
-			kind: 'category',
-			category: newCategory._id,
-		}, {
-			kind: 'category',
-			category: secondCat._id,
-		}]
-	})).save();
-});
+		return (new Menu({
+			name: 'main',
+			links: [{
+				kind: 'category',
+				category: newCategory._id,
+			}, {
+				kind: 'category',
+				category: secondCat._id,
+			}]
+		})).save();
+	})
+	.then(() => {
+		console.log('content created successfully');
+		process.exit(0);
+	});
