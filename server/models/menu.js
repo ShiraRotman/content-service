f0995eb3a793baa9c5551425d37146930030867f
David Meir-Levy <davidmeirlevy@gmail.com>
2019-09-10 16:38:21 +0300

    menu model - add absolute links with new window

--- server/models/menu.js
+++ server/models/menu.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // define the model schema
 const MenuSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		unique: true,
-	},
-	links: [{
-		kind: {
-			type: String,
-			enum: ['category', 'post', 'http'],
-		},
-		category: {type: mongoose.Schema.Types.ObjectId, ref: 'Category'},
-		post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
-		value: String,
-	}],
-});
+  name: {
+    type: String,
+    unique: true,
+  },
+  links: [{
+    kind: {
+      type: String,
+      enum: ['category', 'post', 'http'],
+    },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+    value: { text: String, url: String, newWindow: Boolean },
+  }],
+})
 
-module.exports = mongoose.model('Menu', MenuSchema);
+module.exports = mongoose.model('Menu', MenuSchema)
