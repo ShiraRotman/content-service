9add0ccc0a08e398861f6701f654632e40fa2c5b
David Meir-Levy <davidmeirlevy@gmail.com>
2020-05-02 14:22:26 +0300

    use redis for cache with url

--- server/utils/cache-manager.js
+++ server/utils/cache-manager.js
@@ -2,6 +2,7 @@ const { redisUrl } = require('../../config')
 
 const cacheManager = require('cache-manager').caching({
   store: redisUrl ? require('cache-manager-redis') : 'memory',
+  url: redisUrl,
   ttl: 600,
 })
 
