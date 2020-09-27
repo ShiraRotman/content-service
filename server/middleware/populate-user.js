241cbe417def9651d214e947558bac4d33fef137
David Meir-Levy <davidmeirlevy@gmail.com>
2020-09-27 23:54:52 +0300

    reading user using api-kit middlewares

--- server/middleware/populate-user.js
+++ server/middleware/populate-user.js
@@ -1,32 +1,16 @@
 const { editorsRoles, adminRole } = require('../../config')
-const cacheManager = require('../utils/cache-manager')
-const callAuthService = require('../utils/call-auth-service')
-
-const cachePrefix = 'userByToken:'
-
-function getUser (authorization) {
-  return callAuthService('/api/me', { headers: { authorization } }).then(user => {
-    user.isEditor = user.roles.some(role => editorsRoles.includes(role))
-    user.isAdmin = user.roles.includes(adminRole)
-    return user
-  })
-}
+const { populateUser } = require('@greenpress/api-kit/user-middlewares')
 
 /**
  *  Populate user on request
  */
 module.exports = (req, res, next) => {
-  if (!req.headers.authorization) {
-    next()
-    return
-  }
-  const authorization = req.headers.authorization
-
-  cacheManager.wrap(cachePrefix + authorization, () => getUser(authorization))
-    .then(user => {
-      req.user = user
-      next()
-    }).catch(() => {
-    next()
-  })
+	populateUser(req, res, () => {
+		if (req.user) {
+			const user = req.user
+			user.isEditor = user.roles.some(role => editorsRoles.includes(role))
+			user.isAdmin = user.roles.includes(adminRole)
+		}
+		next()
+	})
 }
